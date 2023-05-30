befbe8fa4245d04df84ce1c2a5287883ee838dea
pannan-annalect <pannan.seran@annalect.com>
2023-05-30 19:14:48 +0530

    Header and Footer changes

---
+++ my-react-app/src/components/footer/footer.component.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Grid, Box, Container,Link,IconButton, InputAdornment, TextField, Typography } from '@mui/material'
+import './footer.scss';
+const FooterComponent = () => {
+    return(
+        <>
+            <footer className="footer">
+                <Container className="container">
+                    <Grid container >
+                        <Grid item md={4} xs={12} sx={{py:1}}>
+                            <Typography variant="h6">
+                                Get to know us 
+                            </Typography>
+                            <ul className="footer-links">
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                            </ul>
+                        </Grid>
+                        <Grid item md={4} xs={12} sx={{py:1}}>
+                            <Typography variant="h6">
+                                Get to know us 
+                            </Typography>
+                            <ul className="footer-links">
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="#">About Us</Link>
+                                </li>
+                            </ul>
+                        </Grid>
+                        <Grid item md={4} xs={12} sx={{py:1}}>
+                            <Typography variant="h6">
+                                Stay updated
+                            </Typography>
+                            <Typography>
+                                Follow us
+                            </Typography>
+                            <Typography>
+                                &copy; 2023 Lorem
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </footer>
+        </>
+    )
+}
+
+export default FooterComponent;
