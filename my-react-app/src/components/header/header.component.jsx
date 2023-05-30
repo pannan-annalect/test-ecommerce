253eaa2aec064bc831fbaf1bfb0d187d9d1b4a33
pannan-annalect <pannan.seran@annalect.com>
2023-05-30 12:18:57 +0530

    Initial commit to include header

---
+++ my-react-app/src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AccountCircle } from '@mui/icons-material';
+import { Grid, IconButton, InputAdornment, TextField, Typography } from '@mui/material'
+
+
+const headerComponent = () => {
+    return(
+        <>
+        test
+        </>
+    )
+}
+
+export default headerComponent
