51c6b6c82794c33efe376d494a78d3dfbf65ff93
Vikas Kapse <vikas.kapse@annalect.com>
2023-06-06 19:37:10 +0530

    created all product list with filter with responsive

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ import FooterComponent from './components/footer/footer.component';
 import AboutUs from './components/about-us/AboutUs.comopnent';
 import ContactUs from './components/contact-us/ContactUs.comopnent';
 import LoginSignUp from './components/login-signup/LoginSIgnUp.component';
-import AllProductListing from './components/product-listing/AllProductListing.component';
+import AllProductsListing from './routes/All-Products/AllProducts.component';
 const theme = createTheme({
   typography:{
     "fontFamily":'"Roboto","sans-serif"'
@@ -50,7 +50,7 @@ function App() {
               <Route path="/about-us" element={<AboutUs/>} />
               <Route path="/contact-us" element={<ContactUs/>} />
               <Route path="/auth" element={<LoginSignUp/>} />
-              <Route path="/all-product" element={<AllProductListing/>} />
+              <Route path="/all-product" element={<AllProductsListing/>} />
             </Routes>
           <FooterComponent />
       </Router>
---
+++ my-react-app/src/routes/All-Products/AllProducts.component.jsx
@@ -0,0 +1,10 @@
+import AllProductListing from "../../components/product-listing/AllProductListing.component"
+
+function AllProductsListing() {
+
+    return(
+        <AllProductListing/>
+    )
+}
+
+export default AllProductsListing;
